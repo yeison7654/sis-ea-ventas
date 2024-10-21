bd6ad0db492ad69d56f6b9ecb579c556c1a5b6fa
Yeison Danner carhuapoma Dett <51946409+yeison7654@users.noreply.github.com>
2024-10-21 16:34:32 -0500

    js

--- App/assets/js/Login/functions_login.js
+++ App/assets/js/Login/functions_login.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-   alert("Hello World!");
+  login();
 });
+
+function login() {
+  let form = document.getElementById("form_login");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let data = new FormData(form);
+    let headers = new Headers();
+    let config = {
+      method: "POST",
+      headers: headers,
+      node: "cors",
+      cache: "no-cache",
+      body: data,
+    };
+    let url = base_url + "Logic/login/login.php";
+    fetch(url, config)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            "Error! status:" + response.status + " - " + response.statusText
+          );
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+}
--- App/views/Login/login-view.php
+++ App/views/Login/login-view.php
@@ -10,18 +10,21 @@
 <body>
     <div style="margin: auto; width: 50%; background-color: aqua; padding: 10px;">
         <h1>Inicio de Sesion</h1>
-        <form>
+        <form id="form_login">
             <div style="margin-bottom: 1rem;">
-                <label for=""></label>
+                <label for="">Usuario</label>
                 <input type="text">
             </div>
             <div style="margin-bottom: 1rem;">
-                <label for=""></label>
+                <label for="">Contraseña</label>
                 <input type="password">
             </div>
             <div style="margin-bottom: 1rem;"><button type="submit">Inicio de Sesion</button></div>
         </form>
     </div>
+    <script>
+        let base_url="<?= BASE_URL ?>";
+    </script>
     <script src="<?= BASE_URL ?>App/assets/js/Login/functions_login.js"></script>
 </body>
 
